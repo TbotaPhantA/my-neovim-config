abe42737ad1351781dfb66f5662e8bb71bf5b388
TbotaPhantA <botakiller@mail.ru>
2024-08-20 12:57:15 +0000

    telescope next/prev mapping was changed

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,3 +1,15 @@
+local actions = require "telescope.actions"
+require('telescope').setup{
+  defaults = {
+    mappings = {
+      i = {
+        ["<A-j>"] = actions.move_selection_next,
+        ["<A-k>"] = actions.move_selection_previous,
+      }
+    }
+  },
+}
+
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<A-p>', builtin.git_files, {})
