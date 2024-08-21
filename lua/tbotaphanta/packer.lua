a9f44d5cf36e425496dfd8a99ec9ccaeb991aa0b
TbotaPhantA <botakiller@mail.ru>
2024-08-21 18:30:20 +0000

    nvim-cmp was added and configured

--- lua/tbotaphanta/packer.lua
+++ lua/tbotaphanta/packer.lua
@@ -50,5 +50,13 @@ return require('packer').startup(function(use)
     end
   })
 
-  -- lsp
+  --nvim-cmp
+  use {'hrsh7th/cmp-buffer'}
+  use {'hrsh7th/cmp-path'}
+  use {'hrsh7th/cmp-cmdline'}
+  use {'hrsh7th/nvim-cmp'}
+
+  use {'hrsh7th/cmp-vsnip'}
+  use {'hrsh7th/vim-vsnip'}
+  use {'rafamadriz/friendly-snippets'}
 end)
