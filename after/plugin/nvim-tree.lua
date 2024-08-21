2e7e59cf54142d203bacd1557a25c56aef5c0cb3
TbotaPhantA <botakiller@mail.ru>
2024-08-21 23:59:34 +0000

    nvim-tree was added to lsp

--- after/plugin/nvim-tree.lua
+++ after/plugin/nvim-tree.lua
@@ -32,5 +32,21 @@ require("nvim-tree").setup {
   view = {
     width = 50,
   },
+  diagnostics = {
+    enable = true,
+    show_on_dirs = true,
+    show_on_open_dirs = true,
+    debounce_delay = 50,
+    severity = {
+      min = vim.diagnostic.severity.HINT,
+      max = vim.diagnostic.severity.ERROR,
+    },
+    icons = {
+      hint = "",
+      info = "",
+      warning = "",
+      error = "",
+    },
+  },
 }
 
